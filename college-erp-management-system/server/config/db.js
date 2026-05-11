6c440ec5920b9fe9a32799ac7a5c248b5dd6f994
sasatyaprakash-03 <satyaprakash.in33@gmail.com>
2026-05-12 00:53:31 +0530

    Updated project

--- college-erp-management-system/server/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-  const mongoUri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/college_erp";
-  const connection = await mongoose.connect(mongoUri);
-  console.log(`MongoDB connected: ${connection.connection.host}`);
-};
--- college-erp-management-system/server/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { dashboardSummary } from "../controllers/dashboardController.js";
-import { protect } from "../middleware/auth.js";
-
-const router = express.Router();
-
-router.get("/summary", protect, dashboardSummary);
-
-export default router;
